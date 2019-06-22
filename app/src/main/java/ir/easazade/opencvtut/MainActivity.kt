d4a6480437285f863b0e695b81028f3912282d13
alireza <easazade@gmail.com>
2019-06-22 12:17:26 +0430

    BUG FIX - changing opencv build.gradle file plugin form android.application to com.android.library

--- app/src/main/java/ir/easazade/opencvtut/MainActivity.kt
+++ app/src/main/java/ir/easazade/opencvtut/MainActivity.kt
@@ -1,12 +1,17 @@
 package ir.easazade.opencvtut
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import org.opencv.android.OpenCVLoader
 
 class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+    if (OpenCVLoader.initDebug()) {
+      Log.d("opencvLoader", "open cv is loaded successfully")
+    }
   }
 }
